18534f40b13d0bada9f8ab096777cd437c13df8f
Andrea N Goldstein <angoldst@berkeley.edu>
2013-09-15 16:06:55 -0700

    eval calculation works, also set up working parser

--- CalCalc.py
+++ CalCalc.py
@@ -1,7 +1,24 @@
-def calculate(inString):
-    comp = re.compile("[a-zA-Z]")
-    if (comp.search(inString)==None):
-        try:
-            print eval(inString)
-        except IOError, e:
-            pass
+import argparse
+import re
+import urllib
+import urllib2
+
+parser = argparse.ArgumentParser(description='CalCalculate Input')
+parser.add_argument('inString', help ='Need to ask your question')
+parser.add_argument('-t', action='store_true', default=False, dest='askAPI', help='Set -t if you want to push question to API')
+
+
+results = parser.parse_args()
+
+
+
+
+comp = re.compile("[a-zA-Z]")
+if (comp.search(results.inString)==None) and not results.askAPI:
+    try:
+        print eval(str(results.inString))
+    except IOError, e:
+        pass
+
+else:
+	print 'asking API'
